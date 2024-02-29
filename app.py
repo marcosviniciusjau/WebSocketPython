d89e2806cee3ada7ed88edaa7d635af0f08a26e6
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-02-29 15:16:45 -0300

    creating the route to get the qr_code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request, send_file
 from repository.database import db
 from db_models.payment import Payment
+from payments.pix import Pix
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
@@ -16,6 +17,13 @@ def create_payment_pix():
 
     expiration_date = datetime.now() + timedelta(minutes=30)
     new_payment= Payment(value= data['value'], expiration_date= expiration_date)
+    
+    pix_obj = Pix()
+    data_payment_pix= pix_obj.create_payment()
+
+    new_payment.bank_payment_id= data_payment_pix["bank_payment_id"]
+    new_payment.qr_code= data_payment_pix["qr_code_path"]
+
     db.session.add(new_payment)
     db.session.commit()
 
@@ -25,6 +33,10 @@ def create_payment_pix():
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
     return jsonify({"message": "The payment has been confirmed"})
+
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
+def get_image(file_name):
+    return send_file(f"static/img/{file_name}.png", mimetype="image/png")
 
 @app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
--- payments/pix.py
+++ payments/pix.py
@@ -5,12 +5,13 @@ class Pix:
         pass
 
     def create_payment(self):
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
+
         hash_payment = f'hash_payment_{bank_payment_id}'
         img = qrcode.make(hash_payment)
         img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
 
-        return {"payment_bank_id": bank_payment_id,
+        return {"bank_payment_id": bank_payment_id,
                 "qr_code_path":f"qr_code_payment_{bank_payment_id}"
                 }
                 
