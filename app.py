49aa3469a920fbffa5ace93b5612004a378d1faa
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-03-01 15:19:25 -0300

    creating the union to the backend routes with frontend

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from flask import Flask,jsonify,request, send_file, render_template
+from flask_socketio import SocketIO
+
 from repository.database import db
 from db_models.payment import Payment
 from payments.pix import Pix
@@ -9,6 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketIo= SocketIO(app)
+
 @app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
     data= request.get_json()
@@ -26,12 +30,27 @@ def create_payment_pix():
 
     db.session.add(new_payment)
     db.session.commit()
-
     return jsonify({"message": "The payment has been created",
                     "payment": new_payment.to_dict()})
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
+    data = request.get_json()
+
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment= Payment.query.filter_by(bank_payment_id= data.get('bank_payment_id')).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+    
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment value"}), 400
+    
+    payment.paid= True
+    db.session.commit()
+    socketIo.emit(f"payment-confirmed-{payment.id}")
     return jsonify({"message": "The payment has been confirmed"})
 
 @app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
@@ -42,11 +61,20 @@ def get_image(file_name):
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if payment.paid:
+        return render_template("confirmed_payment.html", 
+                               payment_id=payment.id,
+                               value=payment.value)
+
     return render_template("payment.html", 
                            payment_id=payment.id,
                            value=payment.value,
                            host="http://127.0.0.1:5000",
                            qr_code=payment.qr_code)
 
+@socketIo.on("connect")
+def handle_connect():
+    print("Client connected to the server")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketIo.run(app,debug=True)
